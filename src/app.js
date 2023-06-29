e138c8065b3a0b0e7e015f378eb179b3ddfe0195
Krishna Rathod <88035445+KryshnaRathod@users.noreply.github.com>
2023-06-29 17:13:35 +0530

    Updated app.js

--- src/app.js
+++ src/app.js
@@ -1,32 +1,47 @@
+
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  if (req.method === 'POST') {
-    const chunks = [];
+  if (req.method === 'POST' && req.url === '/') {
+    let data = '';
 
     req.on('data', chunk => {
-      chunks.push(chunk);
+      data += chunk;
     });
 
     req.on('end', () => {
-      const body = Buffer.concat(chunks).toString();
-      const obj = JSON.parse(body);
-      const num1 = obj.num1;
+      try {
+        const payload = JSON.parse(data);
 
-      if (typeof num1 !== 'number' || isNaN(num1)) {
-        res.statusCode = 400;
-        res.end('Invalid payload. Expected a number.');
-      } else {
-        if (num1 % 2 === 0) {
-          res.statusCode = 200;
-          res.end(`The number ${num1} is even.`);
+        if (typeof payload.num1 !== 'number') {
+          res.statusCode = 400;
+          res.setHeader('Content-Type', 'text/plain');
+          res.end('Invalid payload. Expected a number.');
         } else {
-          res.statusCode = 404;
-          res.end(`The number ${num1} is odd.`);
+          const num1 = payload.num1;
+
+          if (num1 % 2 === 0) {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'text/plain');
+            res.end(`The number ${num1} is even.`);
+          } else {
+            res.statusCode = 404;
+            res.setHeader('Content-Type', 'text/plain');
+            res.end(`The number ${num1} is odd.`);
+          }
         }
+      } catch (error) {
+        res.statusCode = 400;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('Invalid payload. Expected a valid JSON.');
       }
     });
+  } else {
+    res.statusCode = 404;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Not Found');
   }
 });
 
 module.exports = server;
+
