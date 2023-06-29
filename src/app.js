ecfcb5db58e8ed13755cf13284fece917f180a71
Krishna Rathod <88035445+KryshnaRathod@users.noreply.github.com>
2023-06-29 17:08:25 +0530

    Updated  app.js

--- src/app.js
+++ src/app.js
@@ -5,34 +5,28 @@ const server = http.createServer((req, res) => {
     const chunks = [];
 
     req.on('data', chunk => {
-      const buf = Buffer.from(chunk);
-      const str = buf.toString();
-      chunks.push(str);
-      const obj = JSON.parse(chunks)
-      const value = obj.num1;
-    
-     // Write the code here to check if the number is odd or even
+      chunks.push(chunk);
+    });
+
+    req.on('end', () => {
+      const body = Buffer.concat(chunks).toString();
+      const obj = JSON.parse(body);
+      const num1 = obj.num1;
+
       if (typeof num1 !== 'number' || isNaN(num1)) {
         res.statusCode = 400;
-        res.setHeader('Content-Type', 'text/plain');
         res.end('Invalid payload. Expected a number.');
       } else {
         if (num1 % 2 === 0) {
           res.statusCode = 200;
-          res.setHeader('Content-Type', 'text/plain');
           res.end(`The number ${num1} is even.`);
         } else {
           res.statusCode = 404;
-          res.setHeader('Content-Type', 'text/plain');
           res.end(`The number ${num1} is odd.`);
         }
       }
-
-   });
+    });
   }
-
-  
 });
-
 
 module.exports = server;
