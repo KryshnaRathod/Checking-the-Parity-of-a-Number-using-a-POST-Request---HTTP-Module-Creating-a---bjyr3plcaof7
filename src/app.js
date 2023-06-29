944650490c60d406f8051c152099012931ae1898
Krishna Rathod <88035445+KryshnaRathod@users.noreply.github.com>
2023-06-29 17:02:12 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -12,6 +12,21 @@ const server = http.createServer((req, res) => {
       const value = obj.num1;
     
      // Write the code here to check if the number is odd or even
+      if (typeof num1 !== 'number' || isNaN(num1)) {
+        res.statusCode = 400;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('Invalid payload. Expected a number.');
+      } else {
+        if (num1 % 2 === 0) {
+          res.statusCode = 200;
+          res.setHeader('Content-Type', 'text/plain');
+          res.end(`The number ${num1} is even.`);
+        } else {
+          res.statusCode = 404;
+          res.setHeader('Content-Type', 'text/plain');
+          res.end(`The number ${num1} is odd.`);
+        }
+      }
 
    });
   }
