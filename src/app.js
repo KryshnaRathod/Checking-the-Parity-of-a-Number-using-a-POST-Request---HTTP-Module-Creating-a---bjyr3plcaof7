914e71500a8073acaf2121716a25accbbedec427
Krishna Rathod <88035445+KryshnaRathod@users.noreply.github.com>
2023-07-19 11:24:08 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,47 +1,38 @@
-
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  if (req.method === 'POST' && req.url === '/') {
-    let data = '';
+  if (req.method === 'POST') {
+    const chunks = [];
 
     req.on('data', chunk => {
-      data += chunk;
-    });
-
-    req.on('end', () => {
-      try {
-        const payload = JSON.parse(data);
-
-        if (typeof payload.num1 !== 'number') {
-          res.statusCode = 400;
-          res.setHeader('Content-Type', 'text/plain');
-          res.end('Invalid payload. Expected a number.');
-        } else {
-          const num1 = payload.num1;
-
-          if (num1 % 2 === 0) {
-            res.statusCode = 200;
-            res.setHeader('Content-Type', 'text/plain');
-            res.end(`The number ${num1} is even.`);
-          } else {
-            res.statusCode = 404;
-            res.setHeader('Content-Type', 'text/plain');
-            res.end(`The number ${num1} is odd.`);
-          }
-        }
-      } catch (error) {
-        res.statusCode = 400;
-        res.setHeader('Content-Type', 'text/plain');
-        res.end('Invalid payload. Expected a valid JSON.');
+      const buf = Buffer.from(chunk);
+      const str = buf.toString();
+      chunks.push(str);
+      const obj = JSON.parse(chunks)
+      const value = obj.num1;
+    
+     // Write the code here to check if the number is odd or even
+      if (isNaN(value)) {
+        // res.statusCode = 400;
+        res.writeHead(400, { "Content-Type": "text/plain" });
+        res.end(`${value} is not a number`);
+      } else if (value % 2 === 0) {
+        // res.statusCode = 200;
+        res.writeHead(200, { "Content-Type": "text/plain" });
+        res.end(`The number ${value} is even`);
+      } else if (value % 2 !== 0) {
+        // res.statusCode = 404;
+        res.writeHead(404, { "Content-Type": "text/plain" });
+        res.end(`The number ${value} is odd`);
       }
-    });
-  } else {
-    res.statusCode = 404;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Not Found');
+
+   });
   }
+
+  
 });
 
+
 module.exports = server;
+
 
